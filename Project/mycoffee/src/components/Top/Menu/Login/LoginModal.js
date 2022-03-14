020e0a25031d3d7d59ce2a0ed18fb6d8c6e00d2f
dongsuseo <poopoo0515@naver.com>
2022-03-14 18:18:16 +0900

    [ETC] 로그인 기능 구현 중

--- Project/mycoffee/src/components/Top/Menu/Login/LoginModal.js
+++ Project/mycoffee/src/components/Top/Menu/Login/LoginModal.js
@@ -1,13 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const LoginModal = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const EmailHandler = (e) => {
+    setEmail(e.target.value);
+  };
+  const PasswordHanlder = (e) => {
+    setPassword(e.target.value);
+  };
+  useEffect(
+    (e) => {
+      setEmail(e.target.value);
+      setPassword(e.target.value);
+    },
+    [email, password]
+  );
+
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+
+    setIsLoggedIn(true);
+    setEmail("");
+  };
+
   return (
     <div>
       <div></div>
-      <form>
-        <input id="Id" placeholder="사용자 이름 또는 email을 입력해 주세요." />
-        <input id="Password" placeholder="비밀번호를 입력해 주세요." />
-        
+      <form onSubmit={onSubmitHandler}>
+        <input
+          id="Id"
+          onChange={EmailHandler}
+          placeholder="사용자 이름 또는 email을 입력해 주세요."
+        />
+        <input
+          id="Password"
+          onChange={PasswordHanlder}
+          placeholder="비밀번호를 입력해 주세요."
+        />
+        <button>로그인</button>
       </form>
     </div>
   );
