3fa5ae79a63e02953d489bb9b9eed6ca99dc130e
dongsuseo <poopoo0515@naver.com>
2022-03-14 17:41:10 +0900

    [ADD} react-cookie설치

--- Project/mycoffee/src/components/Top/Menu/Login/LoginModal.js
+++ Project/mycoffee/src/components/Top/Menu/Login/LoginModal.js
@@ -7,6 +7,7 @@ const LoginModal = () => {
       <form>
         <input id="Id" placeholder="사용자 이름 또는 email을 입력해 주세요." />
         <input id="Password" placeholder="비밀번호를 입력해 주세요." />
+        
       </form>
     </div>
   );
